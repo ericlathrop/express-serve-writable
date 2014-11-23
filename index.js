8fb989df5b12ddf9aaf928e145e5b03695b5db54
Eric Lathrop <eric@ericlathrop.com>
2014-11-23 16:59:59 -0500

    Use serveWritable.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var serveStatic = require("serve-static");
+var serveWritable = require("./lib/serve_writable");
 
 var args = process.argv.slice(2);
 
 var app = express();
 app.use(serveStatic(args[0]));
+app.use(serveWritable(args[0]));
 app.listen(3000);
